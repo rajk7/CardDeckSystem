b0741d50caa0a9546b69411a288e774f3bbc64d9
RAJ <raj@RAJs-MacBook-Pro.local>
2022-06-08 16:54:28 +0530

    CardDeckSystem

---
+++ Assets/CardFramework/Scripts/Component/CardDeck.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CardDeck : MonoBehaviour 
+{
+	[SerializeField]
+	private GameObject _cardPrefab;	
+	
+	public readonly List<Card> CardList =  new List<Card>();							
+
+	public void InstanatiateDeck(string cardBundlePath)
+	{
+		AssetBundle cardBundle = BundleSingleton.Instance.LoadBundle(DirectoryUtility.ExternalAssets() + cardBundlePath);
+		string[] nameArray = cardBundle.GetAllAssetNames();
+				
+		for (int i = 0; i < nameArray.Length; ++i)
+		{
+			GameObject cardInstance = (GameObject)Instantiate(_cardPrefab);
+			Card card = cardInstance.GetComponent<Card>();
+			card.gameObject.name = Path.GetFileNameWithoutExtension(nameArray[ i ]);
+			card.TexturePath = nameArray[ i ];
+			card.SourceAssetBundlePath = cardBundlePath;
+			card.transform.position = new Vector3(0, 10, 0);
+			card.FaceValue = StringToFaceValue(card.gameObject.name);
+			CardList.Add(card);
+		}
+	}
+	
+	private int StringToFaceValue(string input)
+	{
+		for (int i = 2; i < 11; ++i)
+		{
+			if (input.Contains(i.ToString()))
+			{
+				return i;
+			}
+		}
+		if (input.Contains("jack") ||
+		    input.Contains("queen") ||
+		    input.Contains("king"))
+		{
+			return 10;
+		}
+		if (input.Contains("ace"))
+		{
+			return 11;
+		}
+		return 0;
+	}
+
+	public Text AddedCard;
+	public Card AddRandomCard(List<Card> listToRandomize)
+	{
+		int randomNum = Random.Range(0, listToRandomize.Count);
+		//print(randomNum);
+		Card printRandom = listToRandomize[randomNum];
+		CardList.Add(printRandom);
+		Debug.Log("Added Card :-" + printRandom);
+		AddedCard.text = printRandom.ToString();
+
+		//print(printRandom);
+		return printRandom;
+	}
+
+	public void AddCardButton()
+	{
+		AddRandomCard(CardList);
+	}
+
+	public Text RemovedCard;
+	public Card RemoveRandomCard(List<Card> listToRandomize)
+	{
+		int randomNum = Random.Range(0, listToRandomize.Count);
+		//print(randomNum);
+		Card printRandom = listToRandomize[randomNum];
+		CardList.Remove(printRandom);
+		Debug.Log("removed :-" + printRandom);
+		RemovedCard.text = printRandom.ToString();
+		//print(printRandom);
+		return printRandom;
+	}
+
+	public void RemoveCardButton()
+	{
+		RemoveRandomCard(CardList);
+	}
+}
---
+++ Assets/CardFramework/Scripts/UI/DealerUI.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DealerUI : MonoBehaviour
+{
+	private Dealer _dealer;
+
+	public Text FaceValueText { get { return _faceValueText; } }
+	[SerializeField]
+	private Text _faceValueText;
+
+	private void Awake()
+	{
+		_dealer = GameObject.Find("Dealer").GetComponent<Dealer>();
+		_dealer.DealerUIInstance = this;
+	}
+
+	public void Shuffle()
+	{
+		if (_dealer.DealInProgress == 0)
+		{
+			StartCoroutine(_dealer.ShuffleCoroutine());
+		}
+	}
+
+	public void Draw()
+	{
+		if (_dealer.DealInProgress == 0)
+		{
+			StartCoroutine(_dealer.DrawCoroutine());
+		}
+	}
+
+	[SerializeField]
+	private GameObject topCardButton;
+	public void TopMostCard()
+	{
+		TopCardBackButton.SetActive(true);
+		topCardButton.SetActive(false);
+
+		if (_dealer.DealInProgress == 0)
+		{
+			StartCoroutine(_dealer.DrawTopCard());
+		}
+	}
+
+	[SerializeField]
+	private GameObject TopCardBackButton;
+	public void BackTopMost()
+	{
+		TopCardBackButton.SetActive(false);
+		topCardButton.SetActive(true);
+
+		if (_dealer.DealInProgress == 0)
+		{
+			StartCoroutine(_dealer.DrawTopBack());
+		}
+	}
+
+	[SerializeField]
+	private CardDeck _cardDeck;
+	public void StartAddButton()
+	{
+		_cardDeck.InstanatiateDeck("cards");
+		//StartCoroutine(StackCardRangeOnSlot(0, _cardDeck.CardList.Count, _stackCardSlot));
+	}
+
+}
